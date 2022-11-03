22f4e651849fd5dc37428b8b4bce1ef3154b92c5
0magical <marilynventura@outlook.com>
2022-11-03 12:35:51 -0400

    added libraries for other roles

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-con
+const inquirer = require('inquirer')
+const fs = require("fs")
+const Manager = require ("./lib/Manager")
+const Engineer = require ("./lib/Engineer")
+const Intern = require ("./lib/Intern")
+const template= require("./src/template")
+
+function begin () {
+    
+}
